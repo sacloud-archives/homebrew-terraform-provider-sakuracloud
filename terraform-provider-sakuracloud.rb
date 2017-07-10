a8c5b0c851d5b1a8f1c59262715edb40aa49323f
terraform-provider-sakuracloud <yamamoto.febc@gmail.com>
2017-07-10 12:07:28 +0000

    v0.10.4

--- terraform-provider-sakuracloud.rb
+++ terraform-provider-sakuracloud.rb
@@ -1,14 +1,21 @@
 class TerraformProviderSakuracloud < Formula
 
-  _version = "0.10.3"
-  sha256_src = "57106a387f72d03cdf8982e259e16186d1c00ad9ee548d47be9a4bc38096ba92"
+  _version = "0.10.4"
+  sha256_src_darwin = "78f81bdc52d4ce78cd0e8e376646ec77bfa6ce2ae8b1aab26a521bfda84e466a"
+  sha256_src_linux = "09ee5c792612e7044135805819770fb608c4c258065857d658b85f7a721868fe"
 
   desc "Terraform provider plugin for SakuraCloud"
   homepage "https://github.com/sacloud/terraform-provider-sakuracloud"
-  url "https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v#{_version}/terraform-provider-sakuracloud_darwin-amd64.zip"
-  sha256 sha256_src
   head "https://github.com/sacloud/terraform-provider-sakuracloud.git"
   version _version
+
+  if OS.mac?
+    url "https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v#{_version}/terraform-provider-sakuracloud_darwin-amd64.zip"
+    sha256 sha256_src_darwin
+  else
+    url "https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v#{_version}/terraform-provider-sakuracloud_linux-amd64.zip"
+    sha256 sha256_src_linux
+  end
 
   depends_on "terraform" => :run
 
