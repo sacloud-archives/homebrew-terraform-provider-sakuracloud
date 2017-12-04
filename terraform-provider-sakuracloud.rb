35a7ce1b09a877d73c8168c8cd705ac3d87d7372
febc.yamamoto <yamamoto.febc@gmail.com>
2017-12-04 11:13:53 +0900

    Fix bin name

--- terraform-provider-sakuracloud.rb
+++ terraform-provider-sakuracloud.rb
@@ -3,6 +3,7 @@ class TerraformProviderSakuracloud < Formula
   _version = "1.0.0"
   sha256_src_darwin = "31476924fef482e8961a9d9800d4ea3109a3d1f58b23808300730d65c83f92e7"
   sha256_src_linux = "ad1ec61e49e942ce12168085587bc5485b5cb90fb2a1f521b7ae30a52c086b6d"
+  bin_name = "terraform-provider-sakuracloud_#{_version}_x4"
 
   desc "Terraform provider plugin for SakuraCloud"
   homepage "https://github.com/sacloud/terraform-provider-sakuracloud"
@@ -20,7 +21,7 @@ class TerraformProviderSakuracloud < Formula
   depends_on "terraform" => :run
 
   def install
-    bin.install "terraform-provider-sakuracloud"
+    bin.install "terraform-provider-sakuracloud_v1.0.0_x4"
   end
 
   def caveats; <<-EOS.undent
@@ -29,7 +30,7 @@ class TerraformProviderSakuracloud < Formula
     To enable, put following text in "~/.terraformrc":
 
         providers {
-            sakuracloud = "#{bin}/terraform-provider-sakuracloud_#{_version}"
+            sakuracloud = "#{bin}/terraform-provider-sakuracloud_v1.0.0_x4"
         }
 
   EOS
